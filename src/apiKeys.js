c47d92d5428e55a6638fb57397f4f3631388cb8c
MadfoxRJ <149812971+MadfoxRJ@users.noreply.github.com>
2024-02-13 10:46:12 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "d631d249c7ddb3205da8ca66db706078",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
